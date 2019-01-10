678749ae5538a9914ab8c127565e7313e3c2a259
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 19:01:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,15 @@
 def get_first_name_of_season_winner(data, season)
-  winner_full =
-  data.each do |season, 
+  temp_name = ""
+  winner_full = ""
+  data.each do |year, contestant_info|
+    if year == season
+      temp_name = contestant_info["name"]
+      puts temp_name
+      contestant_info.each do |type, detail|
+        if type == "status" && detail == "Winner"
+          winner_full = temp_name
+          winner_full.split[0]
+          
 end
 
 def get_contestant_name(data, occupation)
