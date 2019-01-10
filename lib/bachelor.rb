592a39348d799fa418305920fabf9f3910d2ca21
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 20:07:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,21 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |year, contestant_info|
+    if year.to_s == season
+      contestant_info.each do |contestant|
+        contestant.each do |stat, value|
+          if stat == "name"
+            temp_name = value
+          elsif stat == "status" && value == "Winner"
+            return temp_name.split[0]
+          end
+        end
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
