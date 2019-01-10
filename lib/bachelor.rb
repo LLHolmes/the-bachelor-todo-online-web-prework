03d8c6da1d947d3572708c58373d633a2259ceee
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 20:01:25 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,32 +6,17 @@ def get_first_name_of_season_winner(data, season)
     if year.to_s == season
       puts "in"
       contestant_info.each do |contestant|
-        puts contestant[:name]
-        return "CHECK"
-=begin        
-        temp_name = contestant[:name]
-        if contestant[:status]
-    
-
-    
-    if year == season
-      puts "in"
-      contestant_info.each do |contestant|
-        temp_name = contestant[:name]
-        puts temp_name
-#        if type == "name"
-#          temp_name = detail
-#           puts temp_name
-        if contestant[:status] == "Winner"
-          return "HI"#temp_name.split[0]
+        contestant.each do |stat, value|
+          if stat == "name"
+            temp_name = value
+            puts temp_name
+          elsif stat == "status" && value == "Winner"
+            return temp_name.split[0]
+          end
         end
-=end
       end
-      puts "end -2"
     end
-    puts "end -1"
   end
-  return "end"
 end
 
 def get_contestant_name(data, occupation)
